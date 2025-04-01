4268ffdfad8919b42641aadc692127d5ab2c5739
Michal Mikolas <nanuqcz@gmail.com>
2025-04-01 16:27:39 +0200

    Added Windows Task Scheduler spec file.

--- build.pombo.bat
+++ build.pombo.bat
@@ -1,3 +1,4 @@
 cxfreeze --script pombo.py --target-dir dist --base-name Win32GUI
 copy pombo.conf dist
 copy pombo.lnk dist
+copy pombo_task.xml dist
--- build.pombo_portable.bat
+++
@@ -1,3 +0,0 @@
-cxfreeze --script pombo_portable.py --target-dir dist --base-name Win32GUI
-copy pombo.conf dist
-copy pombo_portable.lnk dist
